c950349728043a37d6fa0af19feb74cd3daeff79
Patrick San Juan <patrickjames_sj@hotmail.com>
2016-10-05 21:56:24 -0700

    Finished up to Week 3.

---
+++ Assets/Scripts/MouseLookY.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseLookY : MonoBehaviour {
+
+    float pitch = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        pitch -= Input.GetAxis("Mouse Y");
+        transform.localRotation = Quaternion.Euler(pitch, 0, 0);
+        pitch = Mathf.Clamp(pitch, -80, 80);
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+    // Use this for initialization
+    public float horizontalKeyboard = 0;
+    public float verticalKeybaord = 0;
+    public float moveSpeed = 5;
+    Rigidbody rb;
+    //Things used for moving our object
+    Vector3 forwardMovement;    //this could be forwards or backwards. With forwad movement, we will only use the Z component.
+    Vector3 sidewaysMovement;   //this could be left or right. We will only use the X component.
+    public Vector3 totalMovement;      //Sum of forwardMovement and sidewaysMovement
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public float yaw = 0;           //Mouse X
+    
+    
+
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        //Physics.gravity = new Vector3(0, -1000f, 0);
+    }
+    // Update is called once per frame
+    void Update() {
+        playerMovement();
+        playerLook();
+        playerJump();
+    }
+    bool isGrounded() {
+        Ray ray = new Ray(transform.position,Vector3.down);
+        RaycastHit raycastHit;
+        Physics.Raycast(ray, out raycastHit, 1);
+
+        if (raycastHit.collider != null && raycastHit.collider.tag == "floor") {
+            print("isGrounded is true");
+            return true;
+        }
+        else {
+            print("isGrounded is false");
+            return false;
+        }
+    }
+
+    void playerJump()
+    {
+        if (Input.GetKey("space") && isGrounded())
+        {
+            rb.AddForce(0, 1000, 0);
+        }
+    }
+
+    void playerLook() {
+        yaw += Input.GetAxis("Mouse X");
+        transform.localRotation = Quaternion.Euler(0, yaw, 0);
+    }
+    void playerMovement() {
+        //runs whatever is in here every frame
+        //so if we we had a game that runs at 60FPS (frames per second)
+        //the code within Update() will execute 60 times a second
+        horizontalKeyboard = Input.GetAxis("Horizontal");
+        verticalKeybaord = Input.GetAxis("Vertical");
+        //movement 
+        forwardMovement = new Vector3(0, 0, verticalKeybaord * moveSpeed);
+        sidewaysMovement = new Vector3(horizontalKeyboard * moveSpeed, 0, 0);
+        //add forward and sideways movement and set that to total movement
+        totalMovement = sidewaysMovement + forwardMovement;
+        //Remember to uncomment 2 lines below
+        totalMovement = transform.TransformDirection(totalMovement);
+        totalMovement = new Vector3(totalMovement.x, rb.velocity.y, totalMovement.z);
+        //set rb.velocity = totalMovement
+        rb.velocity = totalMovement;
+
+        if (Input.GetKey("q"))
+        {
+            print("Q is pressed");
+            moveSpeed = 10;
+        }
+        else
+        {
+            moveSpeed = 5;
+        }
+    }
+}
