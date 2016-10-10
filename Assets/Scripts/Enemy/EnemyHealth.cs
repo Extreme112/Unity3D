fabcd83302b5b380cbcb23614434f68b2b607a97
Patrick San Juan <patrickjames_sj@hotmail.com>
2016-10-09 17:25:52 -0700

    Implemented. Shoot delay, crosshiar, enemyhealth. Homework: Left click and right click are two different firing modes. Note: Ricky moves sideways when pressing forward only, could be due to how moving is implemented.

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -3,18 +3,25 @@ using System.Collections;
 
 public class EnemyHealth : MonoBehaviour {
 
+    int enemyHealth;
 	// Use this for initialization
 	void Start () {
-	
+        enemyHealth = 100;
 	}
-	
 	// Update is called once per frame
 	void Update () {
 	
 	}
-
     public void displayHitMessage()
     {
-        print("Ouch!" + gameObject);
+        print("Thats hurts!" + gameObject);
+    }
+    public void dealDamage(int damageToDeal)
+    {
+        enemyHealth -= damageToDeal;
+        if(enemyHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/Player/PlayerShoot.cs
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -3,38 +3,59 @@ using System.Collections;
 
 public class PlayerShoot : MonoBehaviour {
 
+    float shootTimer;
+    float shootDelay;
 	// Use this for initialization
 	void Start () {
-	
+        shootTimer = 1;
+        shootDelay = shootTimer + Time.time;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        
+	void FixedUpdate () {
+        playerShoot();
 	}
 
-    void FixedUpdate()
+    void playerShoot()  //playerShoot 'function'
     {
-        playerShoot();
+        if (Input.GetButton("Fire1") && Time.time > shootDelay)
+        {
+            //Shooting
+            Ray shootray = GetComponent<Camera>().ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+            RaycastHit hitObject;       //Store the object we hit (if we did hit something)
+            if (Physics.Raycast(shootray, out hitObject))
+            {
+                if(hitObject.collider.tag == "enemy")   //Remember to 'tag' ememy in the scene
+                {
+                    hitObject.transform.SendMessage("displayHitMessage");
+                    hitObject.transform.SendMessage("dealDamage", 10);
+                }
+            }
+            shootDelay = Time.time + shootTimer;
+        }
     }
 
-    void playerShoot()
-    {
-        if(Input.GetButton("Fire1"))
-        {
-            Ray ray = GetComponent<Camera>().ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
-            RaycastHit rayHit;
-            Debug.DrawRay(this.transform.position,ray.direction,Color.red,2f,true);
-            if (Physics.Raycast(ray, out rayHit))
-            {
-                // print("You hit " + rayHit.collider.tag);
-                rayHit.transform.SendMessage("displayHitMessage");
-            }
-            //else
-            //{
-            //    print("You hit nothing. You suck.");
-            //}
-        }
+   
+
+    //void FixedUpdate()
+    //{
+    //    playerShoot();
+    //}
+
+    //void playerShoot()
+    //{
+    //    if(Input.GetButton("Fire1"))
+    //    {
+    //        Ray ray = GetComponent<Camera>().ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+    //        RaycastHit rayHit;
+    //        Debug.DrawRay(this.transform.position,ray.direction,Color.red,2f,true);
+    //        if (Physics.Raycast(ray, out rayHit))
+    //        {
+    //            // print("You hit " + rayHit.collider.tag);
+    //            rayHit.transform.SendMessage("displayHitMessage");
+    //        }
+
+    //    }
         
-    }
+    //}
 }
